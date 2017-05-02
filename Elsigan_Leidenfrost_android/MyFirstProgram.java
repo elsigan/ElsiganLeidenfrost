5bcda56aeb4d9def8f39b446d08537175ea6b0be
elsigan <s1610237003@fh-ooe.at>
2017-05-02 18:02:43 +0200

    added android folder and file

---
+++ Elsigan_Leidenfrost_android/MyFirstProgram.java
@@ -0,0 +1,13 @@
+
+public class MyFirstProgram {
+	public static void main(String[]args) {
+		int numberOfOutputs;                                  //Das Programm fragt den User, welche Anzahl an Textoutputs die: "Output number
+		Out.println("Please enter the number of outputs ");   //*Anzahl die vorher angegeben wurde*: Hello World!" lauten, in einer neuen Zeile
+		numberOfOutputs = In.readInt();                       //ausgegeben werden sollen. Durch die Behebung einiger Syntax-Fehler und ersetzen
+		for (int i= 1; i <= numberOfOutputs; i++) {           //eines "kleiner als" durch "kleiner gleich" wurde das Programm wieder funktionstüchtig gemacht.
+			Out.print("Output number ");
+			Out.print(i);
+			Out.println(": Hello World!");
+		} //for
+	}//main
+}//MyFirstProgram
