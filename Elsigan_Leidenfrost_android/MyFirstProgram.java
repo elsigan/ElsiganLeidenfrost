62a76bd0ae578d36c21be6a1d38a85c1b79e467b
hsaenn <leidenfrost-hannes@gmx.at>
2017-05-02 18:24:09 +0200

    changed file

--- Elsigan_Leidenfrost_android/MyFirstProgram.java
+++ Elsigan_Leidenfrost_android/MyFirstProgram.java
@@ -1,19 +1,20 @@
+import java.io.IOException;
 
 public class MyFirstProgram {
-	public static void main(String[]args) {
+	public static void main(String[]args) throws IOException {
 		int numberOfOutputs;                                  //Das Programm fragt den User, welche Anzahl an Textoutputs die: "Output number
-		Out.println("Please enter the number of outputs ");   //*Anzahl die vorher angegeben wurde*: Hello World!" lauten, in einer neuen Zeile
-		numberOfOutputs = In.readInt();                       //ausgegeben werden sollen. Durch die Behebung einiger Syntax-Fehler und ersetzen
+		System.out.println("Please enter the number of outputs ");   //*Anzahl die vorher angegeben wurde*: Hello World!" lauten, in einer neuen Zeile
+		numberOfOutputs = System.in.read();                       //ausgegeben werden sollen. Durch die Behebung einiger Syntax-Fehler und ersetzen
 		for (int i= 1; i <= numberOfOutputs; i++) {           //eines "kleiner als" durch "kleiner gleich" wurde das Programm wieder funktionstüchtig gemacht.
-			Out.print("Output number ");
-			Out.print(i);
-			Out.println(": Hello World!");
-			Out.println("SEAS!");
+			System.out.print("Output number ");
+			System.out.print(i);
+			System.out.println(": Hello World!");
+			System.out.println("SEAS!");
 		} //for
 	}
 	
 	public void thisIsAnewMethod(){
-		Out.print("This is a new line!");
+		System.out.print("This is a new line!");
 	}
 	
 	//main
